fa1c7ec373828c2f0bdf05d553cc6d98657c6e50
Dron07Stm12 <dronperenen@gmail.com>
2025-04-13 17:43:03 +0300

    dron_event

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,24 +2,29 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        //int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void Button_Clicked(object sender, EventArgs e)
         {
-            count += 20;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
         }
+
+        //private void OnCounterClicked(object sender, EventArgs e)
+        //{
+        //    count += 20;
+
+        //    if (count == 1)
+        //        CounterBtn.Text = $"Clicked {count} time";
+        //    else
+        //        CounterBtn.Text = $"Clicked {count} times";
+
+        //    SemanticScreenReader.Announce(CounterBtn.Text);
+        //}
     }
 
 }
